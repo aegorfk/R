a2b1a630e04f92c91feb1cbacdf9fd0d1804d8fc
Kate Lyapina <aegorfk@rambler.ru>
2015-03-11 17:14:26 +0400

    f

---
+++ bank/install_all.R
@@ -0,0 +1,53 @@
+install.packages("knitr")
+install.packages("xtable")
+install.packages("texreg")
+install.packages("pander")
+install.packages("memisc")
+
+install.packages("lmtest")
+install.packages("sandwich")
+install.packages("erer")
+install.packages("AUC")
+
+install.packages("ggplot2")
+install.packages("GGally")
+install.packages("lattice")
+install.packages("vcd")
+install.packages("hexbin")
+install.packages("sjPlot")
+
+install.packages("dplyr")
+install.packages("reshape2")
+install.packages("broom")
+install.packages("tidyr")
+install.packages("psych")
+
+install.packages("glmnet")
+install.packages("HSAUR")
+install.packages("sgof")
+install.packages("car")
+
+install.packages("spikeslab")
+install.packages("quantreg")
+install.packages("MCMCpack")
+
+install.packages("devtools")
+
+install.packages("caret")
+install.packages("AER")
+install.packages("ivpack")
+
+install.packages("lubridate") # работа с датами
+install.packages("zoo") # временные ряды
+install.packages("xts") # еще ряды
+install.packages("forecast")
+
+install.packages("quantmod") # загрузка с finance.google.com
+install.packages("Quandl") # загрузка с Quandl
+
+# non-CRAN packages:
+
+devtools::install_github("bdemeshev/rlms") # read RLMS data
+devtools::install_github("bdemeshev/sophisthse") # read data from sophist.hse.ru
+
+install.packages("rusquant",repos="http://r-forge.r-project.org", type="source")

3edd71538c5ecdbca459773a8a3cf060b598af2c
Kate Lyapina <aegorfk@rambler.ru>
2015-03-11 21:19:30 +0400

    и

--- bank/Clustering.R
+++ bank/Clustering.R
@@ -7,8 +7,6 @@ library("rCharts")
 library("rgl")
 
 
-
-
 bankruptcy <- read.csv(file="Предприятия-А.csv",stringsAsFactors = FALSE,  header=TRUE, sep=";")
 bankruptcy2 <- read.csv(file="Предприятия-А.csv",stringsAsFactors = FALSE,  header=TRUE, sep=";")
 bankruptcy <- as.data.frame(sapply(bankruptcy, gsub, pattern=",",replacement="."))
@@ -28,9 +26,9 @@ bankruptcy2$Автономность  <- NULL
 bankruptcy2$Оборачиваемость.активов  <- NULL
 
 
-bank <- (nrow(bankruptcy) -1)*sum(apply(bankruptcy_85, 2,var))
+bank <- (nrow(bankruptcy) -1)*sum(apply(bankruptcy, 2,var))
 for (i in 2:20) bank[i]  <- sum(kmeans(bankruptcy, center = i)$withinss)
-plot(1:18, bank, type="b")
+plot(2:20, bank, type="b")
 
 bank_kmeans <- kmeans(bankruptcy, centers=2, iter.max=10, nstart = 210)
 bankruptcy$cluster <- factor(bank_kmeans$cluster)
